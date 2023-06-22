81f2020a5a1276b4b1a6ad36ddc3535951a53b0a
zhang <zhangrongkai2000@163.com>
2023-06-22 22:11:20 +0800

    初步搭建 vue页面跳转

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,35 @@
 import {createApp} from 'vue'
 import App from './App.vue'
+import Login from '/src/view/Login.vue'
+import Chatbot from '/src/view/Chatbot.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import {createRouter, createWebHashHistory} from "vue-router";
 
+const routes = [
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login,
+    },
+    {
+        path: '/chatbot/:username',
+        name: 'Chatbot',
+        component: Chatbot,
+        //props: true,
+    },
+    {
+        path: '/',
+        redirect: '/login',
+    },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
 
 const app = createApp(App)
 app.use(ElementPlus)
+app.use(router)
 app.mount('#app')
---
+++ frontend/src/view/Login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Login Page</h1>
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" v-model="username"/>
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" v-model="password"/>
+        </div>
+        <button @click="handleLogin">Login</button>
+    </div>
+</template>
+
+<script setup>
+import {reactive, ref} from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+let username = ref("");
+let password = ref("");
+function handleLogin(){
+    router.push(`/chatbot/`+username.value)
+}
+
+
+</script>
