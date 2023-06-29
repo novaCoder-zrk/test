d45ae4a857abf843ef84f4833275d7ef0d6dfdeb
novaCoder-zrk <zhangrongkai2000@163.com>
2023-06-29 13:59:47 +0800

    验证码 demo

--- backend/email_sender.py
+++ backend/email_sender.py
@@ -3,43 +3,37 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
-# 设置发件人、收件人和邮件主题
-sender = '540891528@qq.com'
-recipient = 'zhangrongkai2000@163.com'
-subject = 'Test Email'
 
-# 创建一个多部分（multipart）电子邮件对象
-msg = MIMEMultipart()
+class EmailSender:
+    # 设置发件人、收件人和邮件主题
+    def sending(self, recipient='zhangrongkai2000@163.com', verify_code=''):
+        sender = '540891528@qq.com'
+        subject = 'Test Email'
 
-# 添加文本内容
-body = 'This is a test email.'
-msg.attach(MIMEText(body, 'plain'))
+        # 创建一个多部分（multipart）电子邮件对象
+        msg = MIMEMultipart()
 
-# 添加附件
-# with open('attachment.pdf', 'rb') as f:
-#     attachment = MIMEApplication(f.read(), _subtype='pdf')
-#     attachment.add_header('Content-Disposition', 'attachment', filename='attachment.pdf')
-#     msg.attach(attachment)
+        # 添加文本内容
+        body = 'Your verify code is: '+verify_code
+        msg.attach(MIMEText(body, 'plain'))
 
-# 设置电子邮件头部信息
-msg['From'] = sender
-msg['To'] = recipient
-msg['Subject'] = subject
+        # 设置电子邮件头部信息
+        msg['From'] = sender
+        msg['To'] = recipient
+        msg['Subject'] = subject
 
-# 连接到SMTP服务器并发送电子邮件
-try:
-    smtp_server = 'smtp.qq.com'  # 使用Gmail SMTP服务器
-    smtp_port = 587  # SMTP服务器端口
-    smtp_username = '540891528@qq.com'  # Gmail用户名
-    smtp_password = 'cmzyhfwrdvahbbcj'  # Gmail密码
+        # 连接到SMTP服务器并发送电子邮件
+        try:
+            smtp_server = 'smtp.qq.com'
+            smtp_port = 587
+            server = smtplib.SMTP(smtp_server, smtp_port)
+            smtp_password = 'fclioblajhyqztzb'  # Gmail密码g
+            print("server connnect")
+            server.starttls()
+            server.login(sender, smtp_password)
+            server.sendmail(sender, recipient, msg.as_string())
+            server.quit()
 
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    print("server connnect")
-    server.starttls()
-    server.login(smtp_username, smtp_password)
-    server.sendmail(sender, recipient, msg.as_string())
-    server.quit()
-
-    print('Email sent successfully!')
-except Exception as e:
-    print('Error sending email:', e)
+            print('Email sent successfully!')
+        except Exception as e:
+            print('Error sending email:', e)
---
+++ backend/verify_code_handler.py
@@ -0,0 +1,75 @@
+import random
+import pandas as pd
+import datetime
+
+
+
+def generate_verify_code():
+    code_list=[]
+    for i in range(6):    #控制验证码的位数
+        state=random.randint(1,3)   #生成状态码
+        if state==1:
+            first_kind=random.randint(65,90)   #大写字母
+            random_uppercase=chr(first_kind)
+            code_list.append(random_uppercase)
+        elif state==2:
+            second_kinds=random.randint(97,122)  #小写字母
+            random_lowercase=chr(second_kinds)
+            code_list.append(random_lowercase)
+        elif state==3:
+            third_kinds=random.randint(0,9)
+            code_list.append(str(third_kinds))
+    verification_code="".join(code_list)
+    print(verification_code)
+    return verification_code
+
+
+def update_verify_code(username, verify_code):
+    df = pd.read_excel('account.xlsx')
+    mask = df['account'] == username
+
+    if mask.empty:
+        print("no match")
+    else:
+        dt = datetime.datetime.now()  # 获取当前时间
+        print(dt)
+        dt_str = dt.strftime('%Y-%m-%d %H:%M:%S')
+        # print(dt_str)
+        df.loc[mask, 'verify_code'] = verify_code
+        df.loc[mask, 'verify_time'] = dt_str
+        df.to_excel('account.xlsx', index=False)
+
+
+def get_verify_code(username):
+    code = generate_verify_code()
+    update_verify_code(username, code)
+    return code
+
+
+def check_verify_code(username, verify_code):
+    df = pd.read_excel('account.xlsx')
+    mask = df["account"] == username
+    user_data = df.loc[mask]
+    last_verify_time = user_data['verify_time'].values[0]
+    last_verify_time = datetime.datetime.utcfromtimestamp(last_verify_time.astype('O') / 1e9)
+
+    print(last_verify_time)
+    last_verify_code = user_data['verify_code'].values[0]
+    print(last_verify_code)
+    dt_now = datetime.datetime.now()
+    diff = dt_now - last_verify_time
+    if diff < datetime.timedelta(minutes=5):
+        print("时间差小于5分钟")
+        if verify_code == last_verify_code:
+            # 让认证码失效
+            new_verify_time = last_verify_time - datetime.timedelta(minutes=10)
+            df.loc[mask, 'verify_time'] = new_verify_time
+            df.to_excel('account.xlsx', index=False)
+            print("认证成功")
+        else:
+            print("认证失败")
+    else:
+        print("时间差大于等于5分钟")
+
+#code = get_verify_code('user1')
+check_verify_code('user1','cof7Wy')
